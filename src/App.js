3a7600badce7e795e091be32db25185398a78f0e
Naresh Bhosale <nareshbhosale73@gmail.com>
2022-01-09 23:23:08 +0530

    wait

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import React, { Component } from 'react'
 
 export default class App extends Component {
-  c ="jhon"
+  c = "jhon"
   render() {
     return (
       <div>
-        Hello my first class based Component {this.c}
+        <h2>
+          Hello my first class based Component {this.c}
+        </h2>
       </div>
     )
   }
